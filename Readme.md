beba9a3c153e818fc220265ce551aba5e78e5be9
bharathmsd7 <bharathmsd7@outlook.com>
2020-05-29 10:27:12 +0530

    Map updates

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Covid19-Azure 
+
+ 
+
+__Lets Break the Chain__ :smile: 
+
+ 
+
+![alt text](https://www.holbrooklife.com/wp-content/uploads/2020/03/covid-2.jpg) 
+
+ 
+
+The COVID-19 pandemic, also known as the coronavirus pandemic, is an ongoing pandemic of coronavirus disease 2019 (COVID‑19), caused by severe acute respiratory syndrome coronavirus 2 (SARS‑CoV‑2).The outbreak was first identified in Wuhan, China, in December 2019. The World Health Organization declared the outbreak a Public Health Emergency of International Concern on 30 January, and a pandemic on 11 March. As of 20 May 2020, more than 4.96 million cases of COVID-19 have been reported in more than 188 countries and territories, resulting in more than 326,000 deaths. More than 1.88 million people have recovered from the virus. 
+
+ __Screenshots__
+ ![alt text](screenshots/2.png)
+ 
+ ![alt text](screenshots/1.png)
+ 
+ ![alt text](screenshots/3.png)
+ 
+ ![alt text](screenshots/4.png)
--- livelocation.py
+++ livelocation.py
@@ -1,20 +1,46 @@
 import folium 
-  
+from firebase import firebase  
+firebase = firebase.FirebaseApplication('https://covid19-37d86.firebaseio.com/', None)
+
 def create_map(name, loc):
     name = name
-    arr = loc.split(" ")
+    #arr = loc.split(" ")
+    arr = ['11.956764','79.819659']
 
-    my_map3 = folium.Map(location = arr, zoom_start = 15) 
+    my_map3 = folium.Map(location = arr, zoom_start = 10) 
 
     # CircleMarker with radius 
     folium.CircleMarker(location = arr, 
                         radius = 75, popup = name).add_to(my_map3) 
     # Pass a string in popup parameter 
     folium.Marker(arr, 
-                popup = ' Bharath ', tooltip='Bharath').add_to(my_map3) 
+                popup = name, tooltip = name).add_to(my_map3) 
     
-   
+    lat = []
+    log = []
+    uname = []
+    result = firebase.get('user', '')
+    id = []
+    for i in result:
+        id.append(i)
+    
+    for j in id:
+        temp = result[j]
+        lat.append(temp['latitude'])
+        log.append(temp['longitude'])
+        uname.append(temp['username'])
+    
+    for idx, n in enumerate(id):
+        location = []
+        location.append(lat[idx])
+        location.append(log[idx])
+        username = uname[idx]
+        folium.Marker(location, 
+                popup = username , tooltip= username).add_to(my_map3) 
+
+        print("success")
 
 
     my_map3.save("templates/my_map.html") 
 
+#create_map('asfjd;l','15.000 15.000')
